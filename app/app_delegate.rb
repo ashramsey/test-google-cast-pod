169342719c6104efd99adba46670c0ec84409a89
Ashley Ramsey <ashramsey@gmail.com>
2017-06-13 21:34:14 +1000

    Apply fix from Mark Villacampa to get v3.3.0 working

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -10,13 +10,23 @@ class AppDelegate
     @window.rootViewController = navigationController
     @window.makeKeyAndVisible
 
-
+    p 3
     # chromecast setup
-    options = GCKCastOptions.alloc.initWithReceiverApplicationID("5609EA1F")
+    options = GCKCastOptions.alloc.initWithReceiverApplicationID("6522CABE")
     GCKCastContext.setSharedInstanceWithOptions(options)
     GCKLogger.sharedInstance.setDelegate self
 
 
+    p 2
+
     true
+  end
+
+
+  def logMessage(message, fromFunction: function)
+    if true #@enableSDKLogging
+      # Send SDK's log messages directly to the console.
+      NSLog "function: #{function}, message: #{message}"
+    end
   end
 end
